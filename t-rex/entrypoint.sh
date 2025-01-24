e3b3b9467886bfc380bd598e4e792984d6f77a16
wslerry <ghostspell89@yahoo.com>
2025-01-24 15:42:42 +0800

    major fix, add t-rex

---
+++ t-rex/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+export DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db/${POSTGRES_DB}
+
+sleep 10
+
+echo "create configuration file"
+if [ ! -f /opt/config/pg_config.toml ]; then
+    echo "Configuration file not found. Creating configuration file..."
+    t_rex genconfig --dbconn ${DATABASE_URL} > /opt/config/pg_config.toml
+else
+    echo "Configuration file already exists. Skipping creation."
+fi
+
+echo "Start serving t_rex"
+t_rex serve --bind=0.0.0.0 --openbrowser=false --config=/opt/config/pg_config.toml
+
+tail -f /dev/null
